ccf60fd5f279e726cfebb4c105c39ca7a36b71b8
Ghoul-JS <jmanuelgut170@gmail.com>
2023-01-17 19:27:36 -0500

    cree un archivo db y configuré para el deploy

---
+++ db.js
@@ -0,0 +1,28 @@
+const dotenv = require("dotenv")
+const mongoose = require('mongoose')
+dotenv.config({path: "./config.env"})
+
+// const DB = process.env.DATABASE
+const DB_DEP = process.env.DB_DEPLOY
+
+module.exports = () => {
+    const connect = () => {
+        mongoose.connect(
+            DB_DEP,
+            {
+                keepAlive: true,
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            },
+            (err) => {
+                if (err) {
+                    console.log('DB: ERROR !!');
+                } else {
+                    console.log('Conexion correcta!!')
+                }
+            }
+        )
+
+    }
+    connect();
+}
