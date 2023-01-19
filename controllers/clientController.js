59752fed2e801d7d92225684fc9e67ba26737aa4
danicorderoo <soydcordero@gmail.com>
2023-01-19 17:32:07 -0400

    CRUD completo de Clients y Publications

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,64 +1,106 @@
-const Client = require("../models/clientModel")
+const Client = require("../models/clientModel");
 
-exports.createClient = async ( req, res ) => {
+exports.createClient = async (req, res) => {
+  try {
+    const client = req.body;
 
-    try{
-        const client = req.body
+    const { name, mail, phone, dni } = client;
 
-        const { name, mail, phone, dni } = client
-        
-        const newClient = await Client.create({
-            name,
-            mail,
-            phone,
-            dni
-        })
-       
-        res.status(201).json({
-            status:"success",
-            requestedAt:req.requestedAt,
-            data:{
-                client: newClient
-            }
-        })
+    const newClient = await Client.create({
+      name,
+      mail,
+      phone,
+      dni,
+    });
 
-    }catch (error){
-        res.status(400).json({
-            status: "failure",
-            message: error
-        })
+    res.status(201).json({
+      status: "success",
+      requestedAt: req.requestedAt,
+      data: {
+        client: newClient,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failure",
+      message: error,
+    });
+  }
+};
+
+exports.getAllClients = async (req, res) => {
+  const idClient = req.body.id;
+
+  if (idClient) {
+    try {
+      const clients = await Client.find({ _id: idClient });
+      console.log(clients);
+    } catch (error) {
+      res.status(400).json({
+        status: "failure",
+        message: error,
+      });
     }
-}
+  }
 
-exports.getAllClients = async( req, res ) => {
-    const idClient = req.body.id
+  try {
+    const clients = await Client.find({});
 
-    if(idClient) {
-        try {
-            const clients = await Client.find({_id:idClient})
-            console.log(clients);
-        } catch (error) {
-            
-        }
+    if (!clients.length)
+      return res.status(404).send("No hay cliente por aqui :c");
+
+    res.status(201).json({
+      status: "success",
+      requestedAt: req.requestedAt,
+      data: {
+        client: clients,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failure",
+      message: error,
+    });
+  }
+};
+
+exports.deleteClient = async (req, res) => {
+  const idClient = req.params.id;
+
+  try {
+    const client = await Client.deleteOne({
+      _id: idClient,
+    });
+
+    res.status(201).json({
+      status: "success",
+      client: client,
+    });
+
+    console.log(`Ya el Cliente ${idClient} fue eliminado`);
+  } catch (error) {
+    res.status(400).json({
+      status: "failure",
+      message: error,
+    });
+  }
+};
+
+exports.updateClient = async (req, res) => {
+  const body = req.body;
+
+  const { name, mail, phone, id } = body;
+
+  let client = await Client.findOneAndUpdate(
+    id,
+    { name, mail, phone },
+    {
+      new: true,
     }
+  );
 
-    try{
-        const clients = await Client.find({})
-
-        if(!clients.length) return res.status(404).send("No hay cliente por aqui :c")
-
-        res.status(201).json({
-            status:"success",
-            requestedAt:req.requestedAt,
-            data:{
-                client: clients
-            }
-        })
-
-    }catch (error){
-        res.status(400).json({
-            status: "failure",
-            message: error
-        })
-    }
-}
+  res.status(201).json({
+    status: "success",
+    client: client,
+  });
+};
