0a53d7a75b2880ef162a592e68ab2494e976eba0
Ezequiel Luiten <ezequiel.luiten@gmail.com>
2023-01-19 19:02:25 -0300

    another treecontroler update

--- controllers/treeController.js
+++ controllers/treeController.js
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-const Tree = require("../models/threesModel")
-
-exports.createTree = async ( req, res ) => {
-try{
-    const tree = req.body
-
-    const { name } = tree
-    
-    const newTree =  await Tree.create({
-        name,
-    })
-
-    res.status(201).json({
-        status:"Success",
-        requestedAt:req.requestedAt,
-        data:{
-            tree: newTree
-        }
-    })
-}
-
-catch (error){
-    res.status(400).json({
-        status: "failure",
-        message: error
-    })
-}
-}
-
-exports.getAllTrees = async( req, res ) => {
-
-    try{
-        const trees = await Tree.find({})
-        res.status(201).json({
-            status:"success",
-            requestedAt:req.requestedAt,
-            data:{
-                tree: trees
-            }
-        })
-
-    }catch (error){
-        res.status(400).json({
-            status: "failure",
-            message: error
-        })
-    }
-}
-
 const Tree = require("../models/treesModel");
 
 exports.getAllTrees = async (req, res) => {
@@ -93,4 +43,3 @@ exports.createTrees = async (req, res) => {
     });
   }
 };
-
